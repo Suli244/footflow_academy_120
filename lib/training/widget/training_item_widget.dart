e9d091f23f6d0efb947e15333e3191b28497285e
Suli244 <ayilchievsulaiman@gamil.com>
2024-03-26 22:03:49 +0600

    add

---
+++ lib/training/widget/training_item_widget.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:footflow_academy_120/core/fa_colors.dart';
+import 'package:footflow_academy_120/core/fa_motin.dart';
+import 'package:footflow_academy_120/training/model/training_model.dart';
+import 'package:footflow_academy_120/training/widget/youtube_player_widget_dateil.dart';
+
+class TrainingItemWidget extends StatelessWidget {
+  const TrainingItemWidget({
+    super.key,
+    required this.model,
+    this.isActivPremium = false,
+    required this.onPressed,
+  });
+  final TrainingModel model;
+  final bool isActivPremium;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24.w),
+      width: double.infinity,
+      height: 190.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: Stack(
+        children: [
+          YoutubePlayeR(
+            url: model.url,
+          ),
+          Container(
+            width: double.infinity,
+            height: 190.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.r),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(12.r),
+                      margin: EdgeInsets.only(
+                        left: 11.w,
+                        right: 2.w,
+                        bottom: 15.h,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.r),
+                        color: FaColors.blue003870.withOpacity(0.60),
+                      ),
+                      child: Text(
+                        model.name,
+                        style: TextStyle(
+                          fontSize: 15.h,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FaMotion(
+                onPressed: onPressed,
+                child: Container(
+                  padding: EdgeInsets.all(10.r),
+                  margin: EdgeInsets.only(
+                    left: 10.w,
+                    right: 9.w,
+                    bottom: 15.h,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.r),
+                    color: FaColors.blue003870.withOpacity(0.60),
+                  ),
+                  child: Image.asset(
+                    isActivPremium
+                        ? 'assets/icons/training_icon.png'
+                        : 'assets/icons/training_icon_premium.png',
+                    width: 28.w,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
