aa9558b01367827a8e89d0d61989a9e5a65e60dc
Tolibzhon <sidikovtolibjon2000@gmail.com>
2024-03-27 12:58:06 +0600

    add change

---
+++ lib/read_more/read_detaile.dart
@@ -0,0 +1,107 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:footflow_academy_120/core/fa_colors.dart';
+import 'package:footflow_academy_120/read_more/read_content.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ReadDetaile extends StatelessWidget {
+  const ReadDetaile({super.key, required this.model});
+  final ReadModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
+                  ),
+                  child: CachedNetworkImage(
+                    imageUrl: model.image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 340.h,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 340.h,
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey.shade500,
+                          highlightColor: Colors.grey.shade200,
+                          child: Container(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                  ),
+                ),
+                Positioned(
+                  top: 44.h,
+                  child: const BackButton(
+                    color: FaColors.whate,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 24.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.r),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 2.w,
+                    height: 30.h,
+                    color: FaColors.blue14A0FF,
+                  ),
+                  SizedBox(width: 4.w),
+                  Expanded(
+                    child: Text(
+                      model.country != null
+                          ? '${model.name} (${model.country})'
+                          : model.name,
+                      style: TextStyle(
+                        fontSize: 24.h,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.r),
+              child: Text(
+                model.description,
+                style: TextStyle(
+                  fontSize: 16.h,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 40.h),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/read_more/read_list_page.dart
+++ lib/read_more/read_list_page.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:footflow_academy_120/core/fa_colors.dart';
 import 'package:footflow_academy_120/read_more/read_content.dart';
+import 'package:footflow_academy_120/read_more/read_detaile.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ReadListPage extends StatelessWidget {
@@ -16,6 +18,9 @@ class ReadListPage extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         scrolledUnderElevation: 0,
+        leading: const BackButton(
+          color: Colors.black,
+        ),
         title: Text(
           appBarTitle,
           style: TextStyle(
@@ -25,14 +30,22 @@ class ReadListPage extends StatelessWidget {
           ),
         ),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            mainAxisExtent: 180),
-        itemBuilder: (context, index) => const ReadWidget(),
-        itemCount: listModel.length,
+      body: SafeArea(
+        child: GridView.builder(
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.r,
+            vertical: 16.r,
+          ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              mainAxisExtent: 180),
+          itemBuilder: (context, index) => ReadWidget(
+            model: listModel[index],
+          ),
+          itemCount: listModel.length,
+        ),
       ),
     );
   }
@@ -41,33 +54,70 @@ class ReadListPage extends StatelessWidget {
 class ReadWidget extends StatelessWidget {
   const ReadWidget({
     super.key,
+    required this.model,
   });
+  final ReadModel model;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: CachedNetworkImage(
-        imageUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/250px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg',
-        width: MediaQuery.of(context).size.width,
-        height: 180.h,
-        fit: BoxFit.cover,
-        placeholder: (context, url) => ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: SizedBox(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ReadDetaile(model: model),
+          ),
+        );
+      },
+      child: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: CachedNetworkImage(
+              imageUrl: model.image,
+              width: MediaQuery.of(context).size.width,
+              height: 180.h,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 180.h,
+                  child: Shimmer.fromColors(
+                    baseColor: Colors.grey.shade500,
+                    highlightColor: Colors.grey.shade200,
+                    child: Container(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(8.r),
+            padding: EdgeInsets.symmetric(horizontal: 4.r),
             width: MediaQuery.of(context).size.width,
-            height: 180.h,
-            child: Shimmer.fromColors(
-              baseColor: Colors.grey.shade500,
-              highlightColor: Colors.grey.shade200,
-              child: Container(
-                color: Colors.white,
+            height: 32.h,
+            decoration: BoxDecoration(
+              color: FaColors.blue003870.withOpacity(0.6),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(
+                model.name,
+                maxLines: 1,
+                style: TextStyle(
+                  fontSize: 12.h,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
-        ),
-        errorWidget: (context, url, error) => const Icon(Icons.error),
+        ],
       ),
     );
   }
